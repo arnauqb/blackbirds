bb012b61f70d78d0ee9c1f5497904d43300c0f55
Arnau Quera-Bofarull <arnauq@protonmail.com>
2023-05-16 10:56:10 +0100

    added soft max and soft min

---
+++ birds/utils.py
@@ -0,0 +1,23 @@
+import torch
+
+def soft_maximum(a, b, k):
+    """
+    Soft differentiable maximum function.
+
+    Arguments:
+        a (torch.Tensor): First input tensor.
+        b (torch.Tensor): Second input tensor.
+        k (float): Hardness.
+    """
+    return torch.log(torch.exp(k * a) + torch.exp(k * b)) / k
+
+def soft_minimum(a, b, k):
+    """
+    Soft differentiable minimum function.
+    
+    Arguments:
+        a (torch.Tensor): First input tensor.
+        b (torch.Tensor): Second input tensor.
+        k (float): Hardness.
+    """
+    return -soft_maximum(-a, -b, k)
