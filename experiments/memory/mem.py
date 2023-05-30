10ede156e4e23cdb7157e3234ea76aa7561f5fe5
arnauq <arnau.quera@gmail.com>
2023-05-30 16:02:20 +0100

    memory

--- experiments/memory/mem.py
+++ experiments/memory/mem.py
@@ -1,32 +1,43 @@
 from birds.forecast import compute_and_differentiate_forecast_loss
 from birds.models.june import June
 from birds.posterior_estimators import TrainableGaussian
+from memory_profiler import profile
 import torch
 import numpy as np
 import yaml
 
 import sys
 sys.path.append("./experiments/gradient_horizon")
-from june import make_model, make_prior, true_parameters, _all_parameters
+from june import make_model, make_flow4, make_prior, true_parameters, _all_parameters, _all_no_seed_parameters
 
 device = "cpu"
 n_parameters = len(true_parameters) 
-flow = TrainableGaussian(torch.zeros(len(true_parameters)), 0.1) #make_flow4(n_parameters, device)
+means = torch.tensor([-3.5] + list(-1.0 * np.ones(len(_all_parameters)-1)), dtype=torch.float, device=device)
+#means = torch.tensor(list(-1.0 * np.ones(len(_all_parameters)-1)), dtype=torch.float, device=device)
+true_parameters = means.clone()
+#flow = make_flow4(n_parameters, device)
+flow = TrainableGaussian(means, 0.01, device=device) #make_flow4(n_parameters, device)
 prior = make_prior(n_parameters, device)
-config_file = "./examples/june_config.yaml"
-config = yaml.safe_load(open(config_file))
-config["data_path"] = "../gradabm-june/worlds/camden_leisure_1.pkl"
-model = June(config, _all_parameters)
 loss_fn = torch.nn.MSELoss()
 #flow.load_state_dict(torch.load("../best_model.pt", map_location="cuda:0"))
-true_data = model.run_and_observe(true_parameters)
+
 
 def run():
-    compute_and_differentiate_forecast_loss(loss_fn=loss_fn,
-                                        model=model,
-                                        posterior_estimator=flow,
-                                        n_samples = 5,
-                                        observed_outputs=true_data,
-                                        diff_mode="forward",
-                                       )
+    config_file = "./examples/june_config.yaml"
+    config = yaml.safe_load(open(config_file))
+    for days in [10, 50, 100, 300]:
+        config["timer"]["total_days"] = days
+        #config["data_path"] = "../gradabm-june/worlds/camden_leisure_1.pkl"
+        config["data_path"] = "/cosma7/data/dp004/dc-quer1/gradabm_june_graphs/camden_leisure_1.pkl"
+        config["system"]["device"] = device
+        model = June(config, _all_parameters)
+        true_data = model.run_and_observe(true_parameters)
+        compute_and_differentiate_forecast_loss(loss_fn=loss_fn,
+                                            model=model,
+                                            posterior_estimator=flow,
+                                            n_samples = 5,
+                                            observed_outputs=true_data,
+                                            diff_mode="reverse",
+                                           )
 
+run()
