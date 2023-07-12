fbc63bfe2c3c2e57f8314b825374e310c610b7d4
joelnmdyer <joelnmdyer@gmail.com>
2023-07-12 19:29:38 +0100

    Tests appear to be working

--- test/infer/test_mcmc.py
+++ test/infer/test_mcmc.py
@@ -17,7 +17,8 @@ class TestMALA:
         # Create data
         torch.manual_seed(0)
         sigma = 1.
-        normal_data = torch.distributions.normal.Normal(-1., sigma).sample((3,))
+        data_size = 3
+        normal_data = torch.distributions.normal.Normal(-1., sigma).sample((data_size,))
 
         # Create prior
         mu_0, sigma_0 = 1., 2.
@@ -41,23 +42,24 @@ class TestMALA:
         post_samples = sampler.run(
             torch.tensor([thinned_trial_samples.mean()]), normal_data, scale=scale
         )
-        thinned_post_samples = post_samples[::20]
+        thinned_post_samples = torch.stack(post_samples)[::20]
 
         # Compare to true posterior – estimate KL divergence
-        factor = (1/sigma_0**2 + normal_data.size/sigma**2)
+        factor = (1/sigma_0**2 + data_size/sigma**2)
         true_mean = (mu_0/sigma_0**2 + normal_data.sum()/sigma**2)/factor
-        true_std  = 1./torch.sqrt(factor)
+        true_std  = 1./torch.sqrt(torch.Tensor([factor]))
         obtained_mean = thinned_post_samples.mean()
         obtained_std  = thinned_post_samples.std()
         ratio_stds = obtained_std/true_std
-        assert np.isclose(0.5 * ( 
+        kl = 0.5 * ( 
             (ratio_stds)**2 
             - 1 
             + ((true_mean - obtained_mean)/true_std)**2 
             + 2 * torch.log(1./ratio_stds)
-        ), 0.)
+        )
+        assert np.isclose(kl, 0., atol=1e-3)
 
-    def test_multivariate_normal(self, mv_normal_data):
+    def test_multivariate_normal(self):
         """
         Tests inference on conjugate bivariate Normal example.
         """
@@ -67,6 +69,7 @@ class TestMALA:
         sigma = torch.tensor([[1., 0.4,], [0.4, 2.]])
         true_mean = torch.tensor([-1., 2.])
         true_density = torch.distributions.multivariate_normal.MultivariateNormal(true_mean, sigma)
+        data_size = 3
         normal_data = true_density.sample((3,))
 
         # Create prior
@@ -76,12 +79,13 @@ class TestMALA:
         # Create negative log-likelihood function
         def negative_log_likelihood(theta, data):
             dist = torch.distributions.multivariate_normal.MultivariateNormal(theta, sigma)
-            return - dist.log_prob(data).sum()
+            nll = - dist.log_prob(data).sum()
+            return nll
 
         # Perform trial run
         mala = mcmc.MALA(normal_prior, negative_log_likelihood, w=1.)
         sampler = mcmc.MCMC(mala, 10_000)
-        trial_samples = sampler.run(mu_0, normal_data)
+        trial_samples = sampler.run(mu_0, normal_data, scale=1e-1)
         thinned_trial_samples = torch.stack(trial_samples)[::10].T
         cov = torch.cov(thinned_trial_samples)
         init_state = thinned_trial_samples.mean(dim=1)
@@ -90,14 +94,14 @@ class TestMALA:
         mala = mcmc.MALA(normal_prior, negative_log_likelihood, w=1.)
         sampler = mcmc.MCMC(mala, 20_000)
         post_samples = sampler.run(init_state, normal_data, covariance=cov)
-        thinned_post_samples = post_samples[::20]
+        thinned_post_samples = torch.stack(post_samples[::20])
 
         # Compare to true posterior – estimate KL divergence
         inv_sigma_0 = torch.inverse(sigma_0)
         inv_sigma = torch.inverse(sigma)
         inv_additions = torch.inverse(inv_sigma_0 + data_size*inv_sigma)
         true_mean = torch.matmul(inv_additions,
-                                 (torch.matmul(inv_sigma_0, mu_0) + data_size*torch.matmul(inv_sigma, data.mean(dim=0))))
+                                 (torch.matmul(inv_sigma_0, mu_0) + data_size*torch.matmul(inv_sigma, normal_data.mean(dim=0))))
         true_inv_cov  = inv_sigma_0 + data_size*inv_sigma
         obtained_mean = thinned_post_samples.mean(dim=0)
         obtained_cov  = torch.cov(thinned_post_samples.T)
@@ -113,7 +117,7 @@ class TestMALA:
                 - torch.logdet(true_inv_cov)
         )
 
-        assert np.isclose(kl, 0.)
+        assert np.isclose(kl, 0., atol=1e-2)
 
     def test_random_walk(self):
 
@@ -133,9 +137,8 @@ class TestMALA:
             p = torch.sigmoid(logit_p)
             log1mp = torch.log(1. - p)
             T = y.shape[0]
-            ll = (T - 1) * log1mp
             logp = torch.log(p)
-            ll += ((y[1:] - y[:-1] + 1)/ 2.).sum() * (logp - log1mp)
+            ll = (T - 1) * log1mp + ((y[1:] - y[:-1] + 1) / 2.).sum() * (logp - log1mp)
             return -ll
 
         # Trial run of MCMC
@@ -143,7 +146,7 @@ class TestMALA:
                  w=1., gradient_clipping_norm=np.inf)
         sampler = mcmc.MCMC(mala, 10_000)
         trial_samples = sampler.run(
-            true_data, (true_p + 0.2).unsqueeze(0), scale=1.
+            (true_p + 0.2).unsqueeze(0), true_data, scale=1.
         )
         thinned_trial_samples = torch.stack(trial_samples)[::10].T
         cov = torch.cov(thinned_trial_samples)
@@ -152,10 +155,10 @@ class TestMALA:
         mala = mcmc.MALA(prior, negative_log_likelihood,
                          w=1., gradient_clipping_norm=np.inf)
         sampler = mcmc.MCMC(mala, 20_000)
-        post_samples = sampler.run(true_data, thinned_trial_samples.mean(dim=1),
+        post_samples = sampler.run(thinned_trial_samples.mean(dim=1), true_data,
                                    covariance=cov.unsqueeze(0).unsqueeze(0))
 
         # Large number of data points so _should_ get good agreement between
         # mean and MLE (by Bernstein-von-Mises theorem?)
         est_p = torch.logit( ((true_data[1:] - true_data[:-1]).mean() + 1.)/2. )
-        assert np.isclose(post_samples.mean(), est_p)
+        assert np.isclose(np.mean(post_samples), est_p, rtol=0.05)
